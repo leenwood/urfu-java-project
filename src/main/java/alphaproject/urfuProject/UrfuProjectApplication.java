963a3517ffc49d7ee52fa38cfd8c7263166f04f6
gperman <g.perman@artsofte.ru>
2023-02-20 14:47:14 +0500

    init repository

---
+++ src/main/java/alphaproject/urfuProject/UrfuProjectApplication.java
@@ -0,0 +1,13 @@
+package alphaproject.urfuProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrfuProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrfuProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alphaproject/urfuProject/UrfuProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package alphaproject.urfuProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrfuProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
