cf89f641e2ed9ed5234601d71da3a02c26b621b9
leenwood <george200135@gmail.com>
2023-03-09 18:21:37 +0500

    dev-start. Fix

--- src/main/java/alphaproject/urfuProject/MainController.java
+++ src/main/java/alphaproject/urfuProject/MainController.java
@@ -1,6 +1,7 @@
 package alphaproject.urfuProject;
 
 
+import alphaproject.urfuProject.entity.Account;
 import alphaproject.urfuProject.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,13 +15,11 @@ public class MainController {
     private AccountRepository accountRepository;
 
     @GetMapping("/account/check/{accountId}")
-    public boolean checkAccountById(@PathVariable long accountId) {
+    public String checkAccountById(@PathVariable long accountId) {
         var account = accountRepository.getReferenceById(accountId);
-        if(account != null) {
-            return true;
-        } else {
-            return false;
+        if(account == null) {
+            return "not found";
         }
-
+        return "id: " + account.getId() + ". username: " + account.getUsername();
     }
 }
